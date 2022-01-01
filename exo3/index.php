3eb3a7e173a49f88e6c12bdbfe15f1f0d99fc855
Amina <hathamina@gmail.com>
2022-01-02 00:17:14 +0100

    tab de multiblication

--- exo3/index.php
+++ exo3/index.php
@@ -1,4 +1,3 @@
-
    <?php
    include("commun/header.php");
    include("commun/menu.php");
@@ -6,15 +5,22 @@
 
 <h1>Affichage des tables de multiplication</h1>
 
+<form action="#" method="GET">
+<label for="table">Table de multiplication à afficher </label>
+<input type="number" name="table" id="table">
+<input type="submit" value="Valider">
+</form>
 
 <?php
-define("TABLE",3);
-echo "<h2>la table de " . TABLE . "</h2>";
-for($i=1; $i<=10;$i++){
 
- echo $i ."+". TABLE . "=" . $i * 4 . "<br>";
-    
-    }
+
+if(isset($_GET["table"])){
+   echo "<h2>la table de " . $_GET["table"] . "</h2>";
+   for($i=1; $i<=10;$i++){
+      echo $i ."+".  $_GET["table"]. "=" . $i * 4 . "<br>"; 
+     }
+}
+
 
 ?>
 
